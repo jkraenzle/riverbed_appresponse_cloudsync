5b73b7f1a6269eaed5f494962663fd28b01e3757
Joe Kraenzle <jkraenzle@riverbed.com>
2020-10-22 17:04:13 -0400

    Create aws_test.py

---
+++ docker/aws_test.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import os
+from subprocess import call
+
+hostname = os.environ["HOSTNAME"]
+username = os.environ["USERNAME"]
+password = os.environ["PASSWORD"]
+regionfilter = os.environ["REGIONFILTER"]
+servicefilter = os.environ["SERVICEFILTER"]
+hostgroupprepend = os.environ["AWSHOSTGROUPPREPEND"]
+
+args = ["python",
+        "awsipranges.py",
+        "--hostname", hostname,
+        "--username", username,
+        "--password", password,
+        "--regionfilter", regionfilter,
+        "--servicefilter", servicefilter,
+        "--hostgroupprepend", hostgroupprepend,
+        "--checkforupdates"]
+call(args)
